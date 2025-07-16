1222ef1b876774e933b93b50633a986694bc07b5
Vishalsain08 <sainvishal83417@gmail.com>
2025-07-16 13:46:14 +0530

    latest commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // ✅ important for correct asset resolution
   plugins: [react()],
 })
